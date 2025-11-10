ac9bba105a1e5d050296fd662bc3ffc3300059cf
Cocambo <ilyas_akhmadiev@mail.ru>
2025-11-10 15:49:41 +0300

    Initial commit: add all services, docker and readme

---
+++ backend/api-gateway-service/go.mod
@@ -0,0 +1,3 @@
+module github.com/Cocambo/tamle-kazan/backend/api-gateway-service
+
+go 1.25.3
---
+++ backend/user-service/cmd/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "API USER-SERVICE is running!")
+	})
+
+	fmt.Println("Listening on :8081...")
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
