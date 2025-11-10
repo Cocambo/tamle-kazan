5d595e38068eb7e619bcaa89113c73186b4c111a
Cocambo <ilyas_akhmadiev@mail.ru>
2025-11-10 23:06:46 +0300

    create Dockerfile.dev for frontend and change vite.config.js, docker-compose.yml

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,30 +3,40 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vuetify({ autoImport: false })
-  ],
+    plugins: [
+        vue(),
+        vuetify({ autoImport: false })
+    ],
 
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
-      '@styles': fileURLToPath(new URL('./src/assets/styles', import.meta.url)),
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+            '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+            '@styles': fileURLToPath(new URL('./src/assets/styles', import.meta.url)),
+        },
     },
-  },
 
-  css: {
-  preprocessorOptions: {
-    scss: {
-      additionalData: `
-        @use "vuetify/settings" with (
-          $body-font-family: 'Lato', sans-serif,
-          $heading-font-family: 'Cormorant Garamond', serif
-        );
-      `
-    }
-  }
-}
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `
+          @use "vuetify/settings" with (
+            $body-font-family: 'Lato', sans-serif,
+            $heading-font-family: 'Cormorant Garamond', serif
+          );
+        `
+            }
+        }
+    },
+
+    server: {
+        host: true,          // позволяет подключаться извне (через localhost)
+        port: 5173,          // явно указываем порт
+        watch: {
+            usePolling: true,  // нужно для Docker + Windows/WSL, чтобы hot reload работал
+            interval: 100,     // (опционально) уменьшает задержку реакции на изменения
+        },
+    },
 })
