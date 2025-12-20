ef737b10607a018341f841739e18f11da3fff6df
Cocambo <ilyas_akhmadiev@mail.ru>
2025-12-21 01:59:23 +0300

    add CORS in .env and change router

--- backend/api-gateway-service/internal/router/router.go
+++ backend/api-gateway-service/internal/router/router.go
@@ -4,11 +4,21 @@ import (
 	"github.com/Cocambo/tamle-kazan/backend/api-gateway-service/internal/config"
 	"github.com/Cocambo/tamle-kazan/backend/api-gateway-service/internal/middleware"
 	"github.com/Cocambo/tamle-kazan/backend/api-gateway-service/internal/proxy"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter(cfg *config.Config) *gin.Engine {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     cfg.CORS.AllowOrigins,
+		AllowMethods:     cfg.CORS.AllowMethods,
+		AllowHeaders:     cfg.CORS.AllowHeaders,
+		ExposeHeaders:    cfg.CORS.ExposeHeaders,
+		AllowCredentials: cfg.CORS.AllowCredentials,
+		MaxAge:           cfg.CORS.MaxAge,
+	}))
 
 	userProxy := proxy.NewProxy(cfg.UserServiceURL, "/api/user")
 
